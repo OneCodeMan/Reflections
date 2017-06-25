b8c3d70278350c3c9eea36b505c4f5d7e8228f0d
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-25 13:24:44 -0400

    Home page is okay. Front view of card is okay.

--- Reflections/PromptViewController.swift
+++ Reflections/PromptViewController.swift
@@ -1,13 +1,14 @@
-
 import UIKit
 
-class ViewController: UIViewController {
+class PromptViewController: UIViewController {
 
+    @IBOutlet weak var promptTypeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-
 }
 
