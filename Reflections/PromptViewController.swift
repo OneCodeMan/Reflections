9c270c4fbf96daee4f0561703dff2ed2b7e1bef6
Dave Gumba <dave_gumba@Yahoo.com>
2017-06-25 13:48:30 -0400

    added flip effect

--- Reflections/PromptViewController.swift
+++ Reflections/PromptViewController.swift
@@ -1,9 +1,27 @@
 import UIKit
 
 class PromptViewController: UIViewController {
-
-    @IBOutlet weak var promptTypeLabel: UILabel!
+    var flippedView = false
     
+    // MARK: Front view
+    @IBOutlet weak var frontView: UIView!
+    @IBOutlet weak var promptTypeLabel: UILabel!
+    @IBOutlet weak var frontInstructionLabel: UILabel!
+    
+    // MARK: Back view
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var promptTextLabel: UILabel!
+    @IBOutlet weak var backInstructionLabel: UILabel!
+    
+    @IBAction func flipButton(_ sender: Any) {
+        flippedView = !flippedView
+        
+        let fromView = flippedView ? frontView : backView
+        let toView = flippedView ? backView : frontView
+        
+        UIView.transition(from: fromView!, to: toView!, duration: 0.4, options: [.transitionFlipFromLeft, .showHideTransitionViews])
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
