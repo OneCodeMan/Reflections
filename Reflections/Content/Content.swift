aca2162968b7d33dd8f07c694a59df17e34a555d
Dave Gumba <dave_gumba@Yahoo.com>
2017-07-01 11:22:48 -0400

    Organized files in groups

--- Reflections/Content/Content.swift
+++ Reflections/Content/Content.swift
@@ -12,7 +12,10 @@ class Content {
                          "What makes you different?",
                          "Where do you feel most comfortable at?",
                          "When was the last time you cried?",
-                         "What do you wish you could tell your five year old self?"]
+                         "What do you wish you could tell your five year old self?",
+                         "What is something you are scared to admit?",
+                         "What makes you love being human?",
+                         "What are you insecure about?"]
     
     var answersList = ["Write about someone you admire. What do you admire about this person?",
                        "Write a letter to your past self two years back in time.",
@@ -24,6 +27,13 @@ class Content {
                       "I feel that no one can truly understand me.",
                       "I think I am really messed up.",
                       "I am currently happy with what I have.",
-                      "Sometimes, I think about what would happen if I suddenly disappeared."]
+                      "Sometimes, I think about what would happen if I suddenly disappeared.",
+                      "I have been emotionally damaged in the past.",
+                      "I am really good at pretending that I am normal.",
+                      "I think I am emotionally fragile.",
+                      "I am sad most of the time.",
+                      "The thought of being alone is both comforting and terrifying.",
+                      "I can take care of myself.",
+                      "I often compare myself with others."]
     
 }
